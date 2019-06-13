1d4c0e6dbb51843e7cbaab97e3c1ed5376f21d3f
manegminho <darkmaing@dreamwiz.com>
2019-06-13 17:53:21 +0900

    2019-06-13-01

--- resources/views/MainView/MainApp.blade.php
+++ resources/views/MainView/MainApp.blade.php
@@ -6,12 +6,18 @@
           <hr><hr>
             <table width =120 height=10 align="center">
               @guest
+                <td class ="HomeStyle"><i class="fas fa-clipboard-list"></i></td> <td class ="HomeStyle">
+                        <i class="fas fa-server"></i></td><tr>
+                        <td class ="FontStyle">[ 게시판 ]</td> <td class ="FontStyle">[ 가비아 ]</td>
               @else
                 @if(Auth::user()->name == 'root')
-                        <td class ="HomeStyle"><a href="articles"><i class="fas fa-clipboard-list"></i></a></td> <td class ="HomeStyle">
+                        <td class ="HomeStyle"><a href="/articles"><i class="fas fa-clipboard-list"></i></a></td> <td class ="HomeStyle">
                         <i class="fas fa-server"></i></td><tr>
-                        <td class ="FontStyle"><a href="articles">[ 게시판 ]</a></td> <td class ="FontStyle">[ 가비아 ]</td>
+                        <td class ="FontStyle"><a href="/articles">[ 게시판 ]</a></td> <td class ="FontStyle">[ 가비아 ]</td>
                 @else
+                        <td class ="HomeStyle"><i class="fas fa-clipboard-list"></i></td> <td class ="HomeStyle">
+                        <i class="fas fa-server"></i></td><tr>
+                        <td class ="FontStyle">[ 게시판 ]</td> <td class ="FontStyle">[ 가비아 ]</td>
 
                 @endif
 
